65925605492a832e78a0da248cad28b80afcd175
MERC-Roider <ethan.cocodrile@gmail.com>
2026-05-30 15:57:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-<script>
-   function moverCarrusel(direccion) {
+function moverCarrusel(direccion) {
     const carrusel = document.getElementById('carrusel');
     carrusel.scrollBy({ left: direccion * 1515, behavior: 'smooth' });
 }
-    </script>
