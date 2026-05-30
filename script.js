af39a324eadd2eb5f8addadfd40f4a74f8f57309
MERC-Roider <ethan.cocodrile@gmail.com>
2026-05-30 15:51:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+   function moverCarrusel(direccion) {
+    const carrusel = document.getElementById('carrusel');
+    carrusel.scrollBy({ left: direccion * 1515, behavior: 'smooth' });
+}
+    </script>
